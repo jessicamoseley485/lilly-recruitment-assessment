6858015a41d753a693f94bbbf5efea644dceef71
Jessica <jm03022@surrey.ac.uk>
2025-11-20 00:50:58 +0000

    completed code

--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,19 @@
-
-const express = require('express');
-const app = express();
-const port = 4000;
-
-app.use(express.json());
-app.use(express.static('public'))
-
-app.listen(port, () => {
-  console.log(`Server is running on port http://localhost:${port}`);
-});
-
-app.get('/', function(request, response) {
-    response.sendFile(__dirname + '/index.html');
-});
-
-
-
+ //get a dictionary of the name and price of every medicine by fetching it from '/medicines'
+ fetch('http://localhost:8000/medicines')
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      //get the data from the dictionary using the associated keys
+      var key = Object.keys(data);
+      dataValuesList = data[key];
+      //print out a list of the name and price of each medicine that doesn't have any null or blank values
+      dataValuesList.forEach(medicine => {
+        if (medicine.name != '' && medicine.price != null) {
+          const markup = `<li>${medicine.name}: £${medicine.price}</li>`;
+          //the data should be output within the 'ul' tags in 'index.html'
+          document.querySelector(`ul`).insertAdjacentHTML('beforeend', markup);
+        }
+      });
+            
+  })
